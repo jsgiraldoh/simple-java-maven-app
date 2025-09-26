8f409e8a313af3814acec24545027bc5220e278d
jsgiraldoh28 <johan.giraldo@epayco.com>
2025-09-26 10:46:24 -0500

    test change to message, show the container log

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World from Github Actions!";
+    private static final String MESSAGE = "Hello World from Github Actions 2!";
 
     public App() {}
 
